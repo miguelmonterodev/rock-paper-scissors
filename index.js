424b0814aae51e884759d08816697ecb38b767ce
Miguel Montero <192806710+miguelmonterodev@users.noreply.github.com>
2026-06-11 10:29:10 +0100

    Get the computer choice
    Write a function that randomly return one of the options: rock, paper, scissors.
    Test that the function returns one of the options using console.log()

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+// Check if the file is linked correctly
+console.log("Hello, World!")
+
+// Get the computer choice. Write a function that randomly return one of the three options (rock, paper, scissors)
+function getComputerChoice() {
+    let choiceIndex = Math.floor(Math.random() * 3)
+    return options[choiceIndex]
+}
+
+const options = ["rock", "paper", "scissors"]
+
+console.log(getComputerChoice())
