60aa6ead0a3c7ff2a9a9487cdf181df6bf32eac3
Miguel Montero <192806710+miguelmonterodev@users.noreply.github.com>
2026-06-12 17:14:37 +0100

    Remove the logic that plays five rounds

--- index.js
+++ index.js
@@ -38,25 +38,6 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-// Create a function to play the entire game with 5 rounds
-function playGame() {
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    if (humanScore > computerScore) {
-        console.log("You win the game")
-        console.log(`Score: ${humanScore} vs ${computerScore}`)
-    }else if (humanScore < computerScore) {
-        console.log("You lose the game")
-        console.log(`Score: ${humanScore} vs ${computerScore}`)
-    }else {
-        console.log("Draw!")
-        console.log(`Score: ${humanScore} vs ${computerScore}`)
-    }
-}
-
 // Variables
 const options = ["rock", "paper", "scissors"]
 let humanScore = 0
